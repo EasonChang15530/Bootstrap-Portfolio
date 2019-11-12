c1084c3b6cdc468eb77da57cc2f441366a4ab6fc
eason <eason15530@gmail.com>
2019-11-12 10:14:46 -0500

    add smooth scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
 
-  $("#about-me").on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-      scrollTop: $("#navbar-example2").offset().top
-    }, 300);
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
-
 });
