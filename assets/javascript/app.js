aab2d3a01885271531f3fca1f77e464ccb4fe646
EASON <EASON@eason15530.local>
2020-01-12 14:23:30 -0500

    debug display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $(".nav-link").on('click', function (event) {
-
+    var currentBtn = this
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -13,11 +13,13 @@ $(document).ready(function () {
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top-56
       }, 800, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+        // window.location.hash = hash;
+        $(".nav-link").removeClass("active")
+        $(currentBtn).addClass("active")
       });
     } // End if
   });
